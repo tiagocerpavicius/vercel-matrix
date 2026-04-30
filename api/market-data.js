8ad1eab7b8f11eac3af368afca6a8619ad53836b
tiagocerpavicius <tiagocerpa@hotmail.com>
2026-04-30 17:45:11 -0300

    Update market-data.js

--- api/market-data.js
+++ api/market-data.js
@@ -1,5 +1,9 @@
 export default function handler(req, res) {
   res.status(200).json({
-    test: "ok"
+    data: [
+      { ticker: "GGAL", price: 1200, change: 2.5 },
+      { ticker: "YPF", price: 15000, change: -1.2 },
+      { ticker: "AL30", price: 35.2, change: 0.8 }
+    ]
   });
 }
